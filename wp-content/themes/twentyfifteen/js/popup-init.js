862395d03880d03150f4b13c2820cafe1ac6c512
Levi Baldelomar <hackmaster777@gmail.com>
2015-03-05 17:10:16 -0400

    Modificado texto y funcionalidad de algunos botones del wpjobboard
    El wpjobboard (instalado en stating y live) muestra un mensaje cuando un usuario no registrado intenta aplicar un trabajo, de modo que se cambio por javascript el mensaje y ademas se modifico el texto de los botones "Login" y "Register" para que sean "Join" y "Login", ademas de hacer que muestren el popup respectivo en lugar de enviar al usuario a la url de login/registro.

--- wp-content/themes/twentyfifteen/js/popup-init.js
+++ wp-content/themes/twentyfifteen/js/popup-init.js
@@ -1,78 +1,99 @@
 jQuery(document).ready( function() {
-	
-  jQuery(".sign_up").colorbox({
+
+  var colorbox_config = {
     inline: true,
     width: "430px",
     onOpen: function() { 
       var page = jQuery(this).data('page');
       jQuery("#fs_success_page").val(page);
     }
-  });
-  jQuery(".log_in").colorbox({
-    inline: true,
-    width: "430px",
-    onOpen: function() {
-      var page = jQuery(this).data('page');
-      jQuery("#fs_success_page").val(page);
+  };
+
+  jQuery(".sign_up").colorbox(colorbox_config);
+  jQuery(".log_in").colorbox(colorbox_config);
+
+  if (jQuery(".wpjb-job-apply .wpjb-flash-error span").html() == "Only registered members can apply for jobs.") {
+    jQuery(".wpjb-job-apply .wpjb-flash-error span").html("Only community members can see details and apply for jobs.");
+  }
+
+  var wpjb_buttons = jQuery(".wpjb-job-apply > div > a.wpjb-button ");
+
+  jQuery.each( wpjb_buttons, function( key, value ) {
+
+    var wpjb_button = jQuery(value);
+
+    switch(wpjb_button.html()) {
+      case "Login":
+        wpjb_button.addClass(".sign_up");
+        wpjb_button.attr("href", "#sign_up");
+        wpjb_button.html("Join");
+        wpjb_button.colorbox(colorbox_config);
+        break;
+      case "Register":
+        wpjb_button.addClass(".log_in");
+        wpjb_button.attr("href", "#log_in");
+        wpjb_button.html("Login");
+        wpjb_button.colorbox(colorbox_config);
+        break;
     }
   });
 
-	// Ajax Request - Save Data
-	jQuery('#register_button').click( function( evt ) {
+  // Ajax Request - Save Data
+  jQuery('#register_button').click( function( evt ) {
     console.log(jQuery("#fs_success_page").val());
 
-		if (jQuery("#register_form input[name='agree']:checked").val() != 1) {
-			alert("You must agree to the Terms and Conditions!");			
-			return false;
-		}
+    if (jQuery("#register_form input[name='agree']:checked").val() != 1) {
+      alert("You must agree to the Terms and Conditions!");     
+      return false;
+    }
 
-		var security_code = WPURLS.ajax_register_nonce;
+    var security_code = WPURLS.ajax_register_nonce;
 
-		// Creamos el objecto
-		var data = {
-			action: 'save_data_action', // es el nombre de la accion "wp_ajax_save_data_action" sin el "wp_ajax_"
-			username: jQuery("#register_form input[name='username']").val(),
-			password: jQuery("#register_form input[name='password']").val(),
-			email: jQuery("#register_form input[name='email']").val(),
-			firstname: jQuery("#register_form input[name='firstname']").val(),
-			lastname: jQuery("#register_form input[name='lastname']").val(),
-			career_situation: jQuery("#register_form input[name='career-situation']:checked").val(),
-			nonce: security_code
-		}
+    // Creamos el objecto
+    var data = {
+      action: 'save_data_action', // es el nombre de la accion "wp_ajax_save_data_action" sin el "wp_ajax_"
+      username: jQuery("#register_form input[name='username']").val(),
+      password: jQuery("#register_form input[name='password']").val(),
+      email: jQuery("#register_form input[name='email']").val(),
+      firstname: jQuery("#register_form input[name='firstname']").val(),
+      lastname: jQuery("#register_form input[name='lastname']").val(),
+      career_situation: jQuery("#register_form input[name='career-situation']:checked").val(),
+      nonce: security_code
+    }
 
-		jQuery.post(WPURLS.ajaxurl, data, function(response) {
+    jQuery.post(WPURLS.ajaxurl, data, function(response) {
 
-			// Limpiamos y ocultamos la seccion de errores
-			jQuery("#register_form #error_text").html();
-			jQuery(".form-error").hide();
+      // Limpiamos y ocultamos la seccion de errores
+      jQuery("#register_form #error_text").html();
+      jQuery(".form-error").hide();
 
-			if (response.error) {
-				// Mostramos el error
-				jQuery("#register_form #error_text").html(response.error);
-				jQuery("#register_form .form-error").show();
+      if (response.error) {
+        // Mostramos el error
+        jQuery("#register_form #error_text").html(response.error);
+        jQuery("#register_form .form-error").show();
 
         jQuery.colorbox.resize();
 
-				jQuery('html,body').animate({
-					scrollTop: jQuery("#sign_up").offset().top
-				},'fast');
+        jQuery('html,body').animate({
+          scrollTop: jQuery("#sign_up").offset().top
+        },'fast');
 
-			} else {
+      } else {
 
-				if (response.success) {
-					jQuery("#register_form #success_text").html(response.success);
-					jQuery("#register_form .form-success").show();
+        if (response.success) {
+          jQuery("#register_form #success_text").html(response.success);
+          jQuery("#register_form .form-success").show();
 
           jQuery.colorbox.resize();
 
-					jQuery('html,body').animate({
-						scrollTop: jQuery("#sign_up").offset().top
-					},'fast');
-				}
+          jQuery('html,body').animate({
+            scrollTop: jQuery("#sign_up").offset().top
+          },'fast');
+        }
 
-				if (response.registered === true || response.logged_in === true) {
-					setTimeout( function() {
-						//document.location.reload();
+        if (response.registered === true || response.logged_in === true) {
+          setTimeout( function() {
+            //document.location.reload();
             
             if ( jQuery("#fs_success_page").val() ) {
               document.location.href = jQuery("#fs_success_page").val();
@@ -80,13 +101,13 @@ jQuery(document).ready( function() {
               document.location.reload();
             }
             
-						//document.location.href = WPURLS.home;
-						//document.location.href = WPURLS.admin_profile;
-					}, 2000);					
-				}
-			}
-		});
-	});
+            //document.location.href = WPURLS.home;
+            //document.location.href = WPURLS.admin_profile;
+          }, 2000);         
+        }
+      }
+    });
+  });
   
   // Ajax Request - Login Action
   jQuery('#login_button').click( function( evt ) {
